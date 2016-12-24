08af8bc8dd4766b5f04c5d4e7a07abd73c41a390
vasu-dev <vasu.singh002@gmail.com>
2016-12-24 20:42:19 +0530

    audio download code added

--- down_loader.py
+++ down_loader.py
@@ -8,9 +8,10 @@ base_url='https://www.youtube.com'
 url="https://www.youtube.com/results?search_query="
 
 no_of_args = len(sys.argv)
-arguments = str(sys.argv)
+arguments = sys.argv
 
-if arguments[1] == '-v' or arguments[-1] == '-m':
+
+if arguments[1] == '-v' or arguments[1] == '-m':
 	query = '+'.join(arguments[2:])
 else:
 	query = '+'.join(arguments[1:])
@@ -19,14 +20,16 @@ youtube_r = requests.get(url+query)
 youtube_soup = BeautifulSoup(youtube_r.text,'html.parser')
 
 video_links = []
+print arguments[0]
 
 for link in youtube_soup.findAll('a'):
 	if re.findall('^/watch?.+', link.get('href')):
 		video_links.append(link.get('href'))
 
 download_url = base_url+video_links[0]
+print download_url
 
 if arguments[1] == '-m':
-	os.system('youtube-dl --extract-audio --audio-format mp3 '+ download_url)
+	os.system('youtube-dl -x --audio-format mp3 --audio-quality 0 '+ download_url)
 else:
 	os.system('youtube-dl '+download_url)
